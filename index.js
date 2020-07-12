8d6314d754d4c093acd6e527473b86a1c5376727
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-12 23:20:37 +0700

    fix path v2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const Query = require("./server/routes/dialogflow");
+const path = require("path");
 
 require("dotenv").config();
 
