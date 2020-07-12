1c9a32c9a8078ebca86ce27ca4134a2ef0ac4bb0
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-12 23:08:50 +0700

    delete redux_devtools_extension

--- client/src/index.js
+++ client/src/index.js
@@ -19,9 +19,10 @@ const createStoreWithMiddleware = applyMiddleware(
 ReactDOM.render(
   <Provider
     store={createStoreWithMiddleware(
-      Reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      Reducer
+      // ,
+      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      //   window.__REDUX_DEVTOOLS_EXTENSION__()
     )}
   >
     <BrowserRouter>
