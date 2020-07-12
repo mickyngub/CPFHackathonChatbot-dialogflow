b8d38b53aeea90ecc251dd6c3905d0487c904f2b
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-12 15:47:13 +0700

    textQuery function in Chabot.js

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,12 +1,57 @@
 import React from "react";
+import Axios from "axios";
 
 const Chatbot = () => {
+  const textQuery = async (text) => {
+    let conversations = [];
+
+    let conversation = {
+      who: "user",
+      content: {
+        text: {
+          text: text,
+        },
+      },
+    };
+    console.log(conversation);
+
+    // conversations.push(conversation);
+    const textQueryVariables = {
+      text,
+    };
+    try {
+      const response = await Axios.post(
+        "/api/dialogflow/textQuery",
+        textQueryVariables
+      );
+      const content = response.data.fulfillmentMessages[0];
+
+      conversation = {
+        who: "bot",
+        content: content,
+      };
+      console.log(conversation);
+      //   conversations.push(conversation);
+    } catch (error) {
+      conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: "Error occurred",
+          },
+        },
+      };
+      //   conversations.push(conversation);
+      console.log(conversation);
+    }
+  };
+
   const keyPressHandler = (e) => {
     if (e.key === "Enter") {
       if (!e.target.value) {
         return alert("you need to type something!");
       }
-
+      textQuery(e.target.value);
       e.target.value = "";
     }
   };
