1c1043bcac72d6814172eada2b12a53ab244ee04
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-12 16:42:44 +0700

    show messages in chatbox

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -2,9 +2,17 @@ import React, { useEffect } from "react";
 import Axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { saveMessage } from "../_actions/message_actions";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Adb from "@material-ui/icons/Adb";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 
 const Chatbot = () => {
   const dispatch = useDispatch();
+  const messagesFromRedux = useSelector((state) => state.message.messages);
   useEffect(() => {
     eventQuery("welcomeToWebsite");
   }, []);
@@ -102,6 +110,32 @@ const Chatbot = () => {
       e.target.value = "";
     }
   };
+
+  const renderOneMessage = (message, i) => {
+    return (
+      <List key={i} style={{ padding: "1rem" }}>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              {message.who === "bot" ? <Adb /> : <AccountCircle />}
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={message.content.text.text}
+            secondary={message.who}
+          />
+        </ListItem>
+      </List>
+    );
+  };
+  const renderMessage = (returnedMessages) => {
+    if (returnedMessages) {
+      return returnedMessages.map((message, i) => {
+        return renderOneMessage(message, i);
+      });
+    }
+  };
+
   return (
     <div
       style={{
@@ -112,7 +146,9 @@ const Chatbot = () => {
         margin: "auto",
       }}
     >
-      <div style={{ height: 644, width: "100%", overflow: "auto" }}></div>
+      <div style={{ height: 644, width: "100%", overflow: "auto" }}>
+        {renderMessage(messagesFromRedux)}
+      </div>
       <input
         style={{
           margin: 0,
---
+++ client/src/Chatbot/Sections/Message.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import List from "@material-ui/core/List";
+// import ListItem from "@material-ui/core/ListItem";
+// import ListItemText from "@material-ui/core/ListItemText";
+// import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+// import Avatar from "@material-ui/core/Avatar";
+// import Adb from "@material-ui/icons/Adb";
+// import AccountCircle from "@material-ui/icons/AccountCircle";
+
+// export default function Message(props) {
+//   return (
+//     <List key={props.key} style={{ padding: "1rem" }}>
+//       <ListItem>
+//         <ListItemAvatar>
+//           <Avatar>{props.who === "bot" ? <Adb /> : <AccountCircle />}</Avatar>
+//         </ListItemAvatar>
+//         <ListItemText primary={props.text} secondary={props.who} />
+//       </ListItem>
+//     </List>
+//   );
+// }
